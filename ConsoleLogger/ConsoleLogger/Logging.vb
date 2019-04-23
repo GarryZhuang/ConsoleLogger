ea4e9514c0020a9771009f389e5176504f421bce
Garry Zhuang <34046345+GarryZhuang@users.noreply.github.com>
2019-04-23 13:58:25 +1000

    initial upload

---
+++ ConsoleLogger/ConsoleLogger/Logging.vb
@@ -0,0 +1,30 @@
+﻿Imports System.IO
+Public Class Logging
+    Public Shared Function LogToFile(ByVal Prefix As String,
+                              ByVal LogData As String,
+                              ByVal LoggingColor As ConsoleColor,
+                              Optional ByVal BackgroundColor As ConsoleColor = ConsoleColor.Black,
+                              Optional ByVal LogToOwnFile As Boolean = False,
+                              Optional ByVal CustomLogFile As String = "")
+
+        Dim RetValue As String
+
+        Try
+
+            Dim LogText As String = "[" & Prefix & "] " & LogData
+            Console.BackgroundColor = BackgroundColor
+            Console.ForegroundColor = LoggingColor
+            Console.WriteLine(LogText)
+
+            If LogToOwnFile = True Then
+                My.Computer.FileSystem.WriteAllText(CustomLogFile, LogText, True)
+            End If
+
+            RetValue = "1, Logging OK"
+        Catch ex As Exception
+            RetValue = "0, Failed: " & ex.ToString
+        End Try
+
+        Return RetValue
+    End Function
+End Class
---
+++ ConsoleLogger/ConsoleLogger/Win32.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Win32
+    'Open the Console
+    <DllImport("kernel32.dll")> Public Shared Function AllocConsole() As Boolean
+
+    End Function
+
+    'Close the Console
+    <DllImport("kernel32.dll")> Public Shared Function FreeConsole() As Boolean
+
+    End Function
+
+    'Force Import Console
+    <DllImport("kernel32.dll")> Public Shared Function AttachConsole(ByVal dwProcessId As Integer) As Boolean
+
+    End Function
+End Class
